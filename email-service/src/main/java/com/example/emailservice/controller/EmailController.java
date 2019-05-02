8831d47bc2b1e0e3787e9dff0eda3ade38e4e3ec
oumingyuan <oumingyuan@live.com>
2019-05-02 10:24:39 +0800

    增加测试页面

--- email-service/src/main/java/com/example/emailservice/controller/EmailController.java
+++ email-service/src/main/java/com/example/emailservice/controller/EmailController.java
@@ -1,6 +1,5 @@
 package com.example.emailservice.controller;
 
-//import mingyuaninformation.email.service.SendEmail;
 
 import com.example.emailservice.service.SendEmail;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -18,17 +18,18 @@ public class EmailController {
     SendEmail sendEmail;
 
     @PostMapping("/send")
-    public String sendEmail(@RequestBody Map<String, String> map) {
+    public Map<String, Object> sendEmail(@RequestBody Map<String, String> map) {
 
-//        System.out.println("email: " + email);
-//        System.out.println("information: " + information);
         System.out.println("服务端：" + map);
 
         new Thread(() -> sendEmail.sendMail(
                 "Test", map.get("email"), map.get("information")),
                 "send email thread").start();
 
-        return "system have send an email.";
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("result", "system have send an email.");
+
+        return resultMap;
 
 
     }
